85596f384bf93a25c405b72d60d48f4902638f4d
Joseph Tubridy and Rochelle Roberts <rochelle@>
2019-07-01 13:29:33 -0700

    added try catch for low food level

--- src/main.js
+++ src/main.js
@@ -5,13 +5,35 @@ import './styles.css';
 import { HungryBear } from './hungrybear.js';
 
 let grylls = new HungryBear("Grylls");
+grylls.setHunger();
 
 $(document).ready(function() {
   $('#feedBear').click(function(event) {
     event.preventDefault();
-    grylls.feed();
+    //grylls.feed();
 
+
+    function checkAlive(){
+      if(grylls.foodLevel <= 0){
+        return new Error("You're dead!");
+      }else {
+        return true;
+      }
+    }
+
+    try {
+      const aliveTest = checkAlive();
+      if(aliveTest instanceof Error){
+        console.error(aliveTest.message);
+        throw RangeError("You are dead. Food level too low!");
+      }else{
+        console.log("you are alive, good food level");
+        $('#hungerLevel').prepend("<div> The bear's hunger level is " + grylls.foodLevel +"</div>");
+      }
+    } catch (error) {
+      console.error(`Red Alert! Threat level Midnight! ${error.message}`);
+    }
   });
-  $('#hungerLevel').prepend("<div> The bear's hunger level is " + grylls.foodLevel +"</div>");
+
 
 });
