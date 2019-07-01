3b24705c43ba438f35d2a6c134c5ed62a3cd3b06
Joseph Tubridy and Rochelle Roberts <rochelle@>
2019-07-01 13:41:51 -0700

    add try catch for death to both branches

--- src/main.js
+++ src/main.js
@@ -19,7 +19,26 @@ console.log("after "+grylls.foodLevel);
 $("#feedBear2").click(function() {
  // grylls.eatMedium();
  // grylls.feed(12); --- broken because somehow not a function
- $('#hungerLevel').prepend("<div> The bear's hunger level is " + grylls.foodLevel +"</div>");
+ function isAliveCheck() {
+   if (grylls.foodLevel <= 0) {
+     return new Error("You're dead, food level too low");
+   } else {
+     return true;
+   }
+ }
+
+ try {
+   const isAlive = isAliveCheck();
+   if (isAlive instanceof Error) {
+     console.error(isAlive.message);
+     throw RangeError("foodLevel too low");
+   }else {
+     console.log("you're safe");
+     $('#hungerLevel').prepend("<div> The bear's hunger level is " + grylls.foodLevel +"</div>");
+   }
+ } catch (error) {
+    console.error(`Red alert! We have an error: ${error.message}`);
+ }
 });
 
 });
