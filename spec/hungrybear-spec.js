e3561ea56c77b2db0c0b8fec2f6cc3289e9fad20
Joseph Tubridy and Rochelle Roberts <joseph@>
2019-07-01 09:57:14 -0700

    feed button added, tests pass

--- spec/hungrybear-spec.js
+++ spec/hungrybear-spec.js
@@ -17,18 +17,15 @@
 // });
 
 
-beforeEach(function() {
-    jasmine.clock().install();
-    fuzzy.setHunger();
-  });
 
-  import { HungryBear } from './../js/hungrybear.js';
+  import { HungryBear } from './../src/hungrybear.js';
 
   describe('HungryBear', function() {
     let fuzzy = new HungryBear("Fuzzy");
 
     beforeEach(function() {
       jasmine.clock().install();
+      fuzzy.setHunger();
     });
 
     afterEach(function() {
@@ -45,4 +42,20 @@ beforeEach(function() {
       jasmine.clock().tick(3001);
       expect(fuzzy.foodLevel).toEqual(7);
     });
+
+    it('should get very hungry if the food level drops below zero', function() {
+    fuzzy.foodLevel = 0;
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should get very hungry if 10 seconds pass without feeding', function() {
+    jasmine.clock().tick(10001);
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  });
+
+  it('should have a food level of ten if it is fed', function() {
+    jasmine.clock().tick(9001);
+    fuzzy.feed();
+    expect(fuzzy.foodLevel).toEqual(10);
+  });
   });
--- src/main.js
+++ src/main.js
@@ -2,23 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { CoolDate } from './coolDate.js';
+import { HungryBear } from './hungrybear.js';
 
-// $(document).ready(function() {
-//   $('#date-form').submit(function(event) {
-//     event.preventDefault();
-//     let monthInput = parseInt($('#month').val());
-//     let dayInput = parseInt($('#day').val());
-//     let yearInput = parseInt($('#year').val());
-//     let dateInput = new CoolDate(yearInput, monthInput, dayInput);
-//
-//
-//     $("#solution").append("<li> The date " + (monthInput+1) + "/" + dayInput + "/" + yearInput + " is a " + dateInput.getWeekday() + "! </li>");
+let grylls = new HungryBear("Grylls");
 
+$(document).ready(function() {
+  $('#feedBear').click(function(event) {
+    event.preventDefault();
+    grylls.feed();
 
+  });
+  $('#hungerLevel').prepend("<div> The bear's hunger level is " + grylls.foodLevel +"</div>");
 
-    // output.forEach(function(element) {
-    //   $('#solution').append("<li>" + element + "</li>");
-    // });
-//   });
-// });
+});
